bb9d247fa7a3080a289522d659129f7f6bdd6860
Andrey Bogdanov <wnmplayinggod@gmail.com>
2014-11-27 21:37:20 +0300

    Initial commit

---
+++ CG2/CG2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CG2
+//
+//  Created by Andrey Bogdanov on 27.11.14.
+//  Copyright (c) 2014 Andrey Bogdanov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
